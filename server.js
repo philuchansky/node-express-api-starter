0ed143925d1d8d52cbac1c05de949fc204c9e858
Philippe Luchansky <philuchansky@gmail.com>
2017-02-08 10:34:40 -0800

    unignore .env for template setup, connect to Mongo and listen to PORT

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-//
+const
+  dotenv = require('dotenv').load({silent: true}),
+  express = require('express'),
+  app = express(),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  logger = require('morgan'),
+  PORT = process.env.PORT || 3000,
+  MONGODB_URL = process.env.MONGODB_URL
+
+mongoose.connect(MONGODB_URL, (err) => {
+  console.log(err || 'Connected to MongoDB.')
+})
+
+app.listen(PORT, (err) => {
+  console.log(err || `Server running on ${PORT}.`)
+})
